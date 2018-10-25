b9701a164fea146d124c075509fd0962de9dfe62
Tori Jones <Tori@Trystian-Amedee-Joness-MacBook-Pro.local>
2018-10-25 18:40:47 -0500

    done

---
+++ 7_3.js
@@ -0,0 +1,21 @@
+for(var i=1;i<=100;i++){
+    if(i%3 == 1){
+        continue;
+        console.log("Fizz");
+    }
+}
+    for(var i=1;i<=100;i++){
+    if(i%5 == 1){
+        continue;
+        console.log("Buzz");
+    }
+}
+    for(var i=1;i<=100;i++){
+    if(i%3 && i%5 == 1){
+        continue;
+    }
+}
+        console.log("FizzBuzz");
+
+
+    console.log(i);
